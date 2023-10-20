65d99b65fabec35c2548ab6eb1c15141c371ca7a
MEJBMEJB <legendsoo@naver.com>
2023-10-20 18:29:39 +0900

    Update CenterFlame.cs
    필요없는 코드 제거

--- Assets/Script/CenterFlame.cs
+++ Assets/Script/CenterFlame.cs
@@ -11,22 +11,11 @@ public class CenterFlame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //   _myAudio = GetComponent<AudioSource>();
-        //_musicStart = false;
-        StartCoroutine(PlayAndEnd());
-        
+        StartCoroutine(PlayAndEnd());        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (!_musicStart)
-        //{
-        //    if (collision.CompareTag("Note"))
-        //    {
-        //    //    _myAudio.Play();
-        //        _musicStart = true;
-        //    }
-        //}
         if (collision.CompareTag("Note"))
         {
          //   AudioManager.instance.PlayBGM("BGM0");
@@ -36,19 +25,17 @@ public class CenterFlame : MonoBehaviour
 
     IEnumerator PlayAndEnd()
     {
+        //음악 출력 -> 끝날때까지 코루틴으로 기다리기
         //string playBGM = RememberDataBeforeStart.Instance.BGMName;        
         string playBGM = PlayerPrefs.GetString("BGMName");
         AudioManager.instance.PlayBGM(playBGM);
-        //_musicStart = true;
         while (true)
         {
             yield return new WaitForSeconds(1f);
             if(AudioManager.instance.isFinishPlay())
             {
                 AudioManager.instance.StopBGM();
-                //DontDestroyOnLoad(RememberDataBeforeStart.Instance.objStageMenu);
                 SceneManager.LoadScene("StageClear");
-                //Result.ResultInstance.ShowResult();
                 break;
             }
         }
